e451d2a88210cbc2395f56a8bfbd49cd2ba9eb10
Éverton Piceni <318867960+evertonpiceni-sketch@users.noreply.github.com>
2026-09-05 17:08:51 -0300

    fix: load CPF guard before payment flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import './asaas-cpf-fix';
 import './payment-fix';
 import App from './App.tsx';
 import './index.css';
