5279925a2c2c38d7e16ca9ff58831f40918b26f2
Éverton Piceni <318867960+evertonpiceni-sketch@users.noreply.github.com>
2026-09-06 15:20:02 -0300

    fix: restore Pix and card payment options

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './asaas-cpf-fix';
 import './payment-fix';
-import './pix-only';
 import App from './App.tsx';
 import './index.css';
 import './responsive.css';
