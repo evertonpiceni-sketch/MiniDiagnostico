e185f85953eb6e097c51d286b7e14adeb99fcf82
Éverton Piceni <318867960+evertonpiceni-sketch@users.noreply.github.com>
2026-09-06 15:28:50 -0300

    fix: use stable payment screen controller

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './asaas-cpf-fix';
-import './payment-fix';
+import './payment-fix-safe';
 import App from './App.tsx';
 import './index.css';
 import './responsive.css';
