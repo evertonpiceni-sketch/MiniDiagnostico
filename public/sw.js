a765f2308e388a86278834e325e7ee2564cd1397
Éverton Piceni <318867960+evertonpiceni-sketch@users.noreply.github.com>
2026-09-08 13:44:36 -0300

    fix: retire stale service worker caches

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.registration.unregister(),
+      caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
+    ]).then(() => self.clients.matchAll({ type: 'window' }))
+      .then((clients) => Promise.all(clients.map((client) => client.navigate(client.url))))
+  );
+});
