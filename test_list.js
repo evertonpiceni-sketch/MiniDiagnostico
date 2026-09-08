ca660eb321bf6b624aa99aefa3dbf67c0459db58
Éverton Piceni <318867960+evertonpiceni-sketch@users.noreply.github.com>
2026-09-08 20:19:37 -0300

    chore: remove obsolete Stripe test

--- test_list.js
+++
@@ -1,3 +0,0 @@
-const Stripe = require('stripe');
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_123'); // Just to see if method exists
-console.log(typeof stripe.checkout.sessions.list);
